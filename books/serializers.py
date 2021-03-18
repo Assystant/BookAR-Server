f2b029f628861bfc0aaa9bc125b3111ac3d79b40
Saket Shukla <saket@assystant.com>
2021-03-18 16:04:19 +0530

    ARB-6 -Modified the listing Book endpoint so that even all the phrases related to the book are listed

--- books/serializers.py
+++ books/serializers.py
@@ -1,14 +1,19 @@
 from rest_framework import serializers
 from .models import BookModel, PhrasesModel
-class BookSerializer(serializers.ModelSerializer):
 
-    class Meta:
-        model = BookModel
-        fields = ['name', 'published_date', 'book_cover','description',
-                  'ISBN','author','publisher']
-
+#ARB-6
 class PhraseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = PhrasesModel
-        fields = ['phrase', 'book']
+        fields = ['phrase', 'book', 'modified']
+
+
+class BookSerializer(serializers.ModelSerializer):
+    phrases = PhraseSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = BookModel
+        fields = ['name', 'published_date', 'book_cover','description',
+                  'ISBN','author','publisher','phrases','modified']
+
