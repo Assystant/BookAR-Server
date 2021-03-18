f3ae7eb83813383e19c99cf29dca3aeea85f21bb
Saket Shukla <saket@assystant.com>
2021-03-18 15:56:44 +0530

    ARB-4 -Filter Books Based on Phrase

--- books/views.py
+++ books/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .serializers import BookSerializer, PhraseSerializer
 # from . import serializers
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import BookModel, PhrasesModel
 from rest_framework.response import Response
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin
 # Create your views here.
 # class BookViewSet(viewsets.ModelViewSet):
-class BookViewSet(viewsets.GenericViewSet,ListModelMixin,CreateModelMixin):
+class BookViewSet(viewsets.GenericViewSet,ListModelMixin,CreateModelMixin, RetrieveModelMixin, UpdateModelMixin):
     """
     A viewset for viewing and editing user instances.
     """
@@ -17,7 +17,9 @@ class BookViewSet(viewsets.GenericViewSet,ListModelMixin,CreateModelMixin):
 
 
 
-class PhraseViewSet(viewsets.ModelViewSet):
+class PhraseViewSet(viewsets.GenericViewSet,ListModelMixin,CreateModelMixin, RetrieveModelMixin, UpdateModelMixin):
     serializer_class = PhraseSerializer
     queryset = PhrasesModel.objects.all()
-
+    #ARB -4
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['phrase']
